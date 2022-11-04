456b9ec5bcc7aaaafe8ab8a63cc4bc7eedae7bd0
Tyler Oalman <tyleroalman@gmail.com>
2022-11-04 18:59:21 -0500

    Create custom debug impl for Token

--- src/token.rs
+++ src/token.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 pub enum Token {
     Illegal(char),
     Eof,
@@ -40,6 +40,40 @@ pub enum Token {
     If,
     Else,
     Return,
+}
+
+impl fmt::Debug for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Illegal(_) => f.write_fmt(format_args!("Type: Illegal\tLiteral: {self}")),
+            Self::Eof => f.write_fmt(format_args!("Type: Eof\tLiteral: {self}")),
+            Self::Ident(_) => f.write_fmt(format_args!("Type: Ident\tLiteral: {self}")),
+            Self::Int(_) => f.write_fmt(format_args!("Type: Int\tLiteral: {self}")),
+            Self::Assign => f.write_fmt(format_args!("Type: Assign\tLiteral: {self}")),
+            Self::Plus => f.write_fmt(format_args!("Type: Plus\tLiteral: {self}")),
+            Self::Minus => f.write_fmt(format_args!("Type: Minus\tLiteral: {self}")),
+            Self::Bang => f.write_fmt(format_args!("Type: Bang\tLiteral: {self}")),
+            Self::Asterisk => f.write_fmt(format_args!("Type: Asterisk\tLiteral: {self}")),
+            Self::Slash => f.write_fmt(format_args!("Type: Slash\tLiteral: {self}")),
+            Self::Lt => f.write_fmt(format_args!("Type: Lt\tLiteral: {self}")),
+            Self::Gt => f.write_fmt(format_args!("Type: Gt\tLiteral: {self}")),
+            Self::Eq => f.write_fmt(format_args!("Type: Eq\tLiteral: {self}")),
+            Self::NotEq => f.write_fmt(format_args!("Type: NotEq\tLiteral: {self}")),
+            Self::Comma => f.write_fmt(format_args!("Type: Comma\tLiteral: {self}")),
+            Self::Semicolon => f.write_fmt(format_args!("Type: Semicolon\tLiteral: {self}")),
+            Self::LParen => f.write_fmt(format_args!("Type: LParen\tLiteral: {self}")),
+            Self::RParen => f.write_fmt(format_args!("Type: RParen\tLiteral: {self}")),
+            Self::LBrace => f.write_fmt(format_args!("Type: LBrace\tLiteral: {self}")),
+            Self::RBrace => f.write_fmt(format_args!("Type: RBrace\tLiteral: {self}")),
+            Self::Function => f.write_fmt(format_args!("Type: Function\tLiteral: {self}")),
+            Self::Let => f.write_fmt(format_args!("Type: Let\tLiteral: {self}")),
+            Self::True => f.write_fmt(format_args!("Type: True\tLiteral: {self}")),
+            Self::False => f.write_fmt(format_args!("Type: False\tLiteral: {self}")),
+            Self::If => f.write_fmt(format_args!("Type: If\tLiteral: {self}")),
+            Self::Else => f.write_fmt(format_args!("Type: Else\tLiteral: {self}")),
+            Self::Return => f.write_fmt(format_args!("Type: Return\tLiteral: {self}")),
+        }
+    }
 }
 
 impl fmt::Display for Token {
