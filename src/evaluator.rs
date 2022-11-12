fb763a1ea9c5fda84d16c2b3db0eb2d29dfd6d19
Tyler Oalman <tyleroalman@gmail.com>
2022-11-11 18:59:43 -0600

    Evaluate prefix expressions

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,6 +1,7 @@
 use crate::{
     ast::{Expr, Program, Statement},
     object::{self, Object},
+    token::Token,
 };
 
 pub type Result<T> = std::result::Result<T, EvalError>;
@@ -36,7 +37,27 @@ fn eval_expression(expr: &Expr) -> Result<Object> {
                 Ok(object::FALSE)
             }
         }
+        Expr::Prefix(operator, expr) => {
+            let right = eval_expression(expr)?;
+            eval_prefix_expression(operator, right)
+        }
         _ => unimplemented!(),
+    }
+}
+
+fn eval_prefix_expression(operator: &Token, right: Object) -> Result<Object> {
+    match operator {
+        Token::Bang => match right {
+            object::TRUE => Ok(object::FALSE),
+            object::FALSE => Ok(object::TRUE),
+            object::NULL => Ok(object::TRUE),
+            _ => Ok(object::FALSE),
+        },
+        Token::Minus => match right {
+            Object::Integer(int) => Ok(Object::Integer(-int)),
+            _ => Ok(object::NULL),
+        },
+        _ => Ok(object::NULL),
     }
 }
 
@@ -57,16 +78,32 @@ mod tests {
 
     #[test]
     fn test_eval_integer_expression() {
-        let tests = vec![("5", Object::Integer(5)), ("10", Object::Integer(10))];
+        let tests = vec![
+            ("5", Object::Integer(5)),
+            ("10", Object::Integer(10)),
+            ("-5", Object::Integer(-5)),
+            ("-10", Object::Integer(-10)),
+        ];
 
         run_tests!(tests);
     }
 
     #[test]
     fn test_eval_boolean_expression() {
+        let tests = vec![("true", object::TRUE), ("false", object::FALSE)];
+
+        run_tests!(tests);
+    }
+
+    #[test]
+    fn test_bang_operator() {
         let tests = vec![
-            ("true", Object::Boolean(true)),
-            ("false", Object::Boolean(false)),
+            ("!true", object::FALSE),
+            ("!false", object::TRUE),
+            ("!5", object::FALSE),
+            ("!!true", object::TRUE),
+            ("!!false", object::FALSE),
+            ("!!5", object::TRUE),
         ];
 
         run_tests!(tests);
